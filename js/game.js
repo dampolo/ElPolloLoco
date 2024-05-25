fada9a75af31437c00b341d41793f994daf61c8f
Damian Poloczek <damianpoloczek@gmail.com>
2024-05-25 21:57:37 +0200

    game.js docs

--- js/game.js
+++ js/game.js
@@ -35,18 +35,41 @@ const embedElement = document.querySelector("embed");
 const playAgainButtonYouLost = document.querySelector(".play-again");
 const playAgainButtonYouWon = document.querySelector(".play-again-you-won");
 
+/**
+ * Adds a click event listener to the 'musicOnButton' element.
+ * When the 'musicOnButton' element is clicked, it:
+ * - Sets 'soundOn' flag to false.
+ * - Hides the 'musicOnButton' by adding the "d-none" class.
+ * - Shows the 'musicOffButton' by removing the "d-none" class.
+ */
 musicOnButton.addEventListener("click", () => {
     soundOn = false;
     musicOnButton.classList.add("d-none");
     musicOffButton.classList.remove("d-none");
 });
 
+/**
+ * Adds a click event listener to the 'musicOffButton' element.
+ * When the 'musicOffButton' element is clicked, it:
+ * - Sets 'soundOn' flag to true.
+ * - Shows the 'musicOnButton' by removing the "d-none" class.
+ * - Hides the 'musicOffButton' by adding the "d-none" class.
+ */
 musicOffButton.addEventListener("click", () => {
     soundOn = true;
     musicOnButton.classList.remove("d-none");
     musicOffButton.classList.add("d-none");
 });
 
+/**
+ * Begins the game.
+ * - Hides the start screen and game over screen.
+ * - Shows the break button and reset button.
+ * - Hides the start game button and info screen.
+ * - Shows the fullscreen button.
+ * - Initializes the level.
+ * - Gets the canvas element and creates a new World instance.
+ */
 const start = () => {
     startScreen.classList.add("d-none");
     gameOverScreen.classList.add("d-none");
@@ -61,8 +84,18 @@ const start = () => {
     world = new World(canvas, keyboard);
 };
 
+/**
+ * Adds a click event listener to the 'startGame' element.
+ * When the 'startGame' element is clicked, it calls the 'start' function to begin the game.
+ */
 startGame.addEventListener("click", start);
 
+/**
+ * Clears all intervals and timeouts.
+ * Iterates through a large range of interval and timeout IDs (1 to 9999999),
+ * clearing them one by one using clearInterval and clearTimeout functions.
+ * This effectively clears all intervals and timeouts that may have been set.
+ */
 function clearAllIntervals() {
     for (let i = 1; i < 9999999; i++) {
         window.clearInterval(i);
@@ -70,6 +103,19 @@ function clearAllIntervals() {
     }
 }
 
+/**
+ * Resets the game state.
+ * - Resets and pauses the main sound.
+ * - Pauses the 'sleep' sound.
+ * - Shows the start screen and info button.
+ * - Hides the reset button and start game button.
+ * - Hides the break button and start button for break.
+ * - Hides the fullscreen buttons.
+ * - Hides the game over and you won screens.
+ * - Exits fullscreen if the document is in fullscreen mode.
+ * - Clears all intervals.
+ * - Sets 'isGameOn' flag to true.
+ */
 const reset = () => {
     soundManager.resetSound("mainSound");
     soundManager.pauseSound("mainSound");
@@ -91,8 +137,22 @@ const reset = () => {
     isGameOn = true;
 };
 
+/**
+ * Adds a click event listener to the 'resetGame' element.
+ * When the 'resetGame' element is clicked, it calls the 'reset' function to reset the game.
+ */
 resetGame.addEventListener("click", reset);
 
+/**
+ * Adds a click event listener to the 'playAgainButtonYouLost' element.
+ * When the button is clicked, it performs several actions to reset the game after losing:
+ * 1. Removes the "d-none" class from 'buttonsTop', 'buttonsBottom', 'playAgainButtonYouLost', and 'playAgainButtonYouWon', if present.
+ * 2. Removes the "buttons-top-now-show" and "buttons-bottom-now-show" classes from 'buttonsTop' and 'buttonsBottom', respectively.
+ * 3. Sets 'isGameOn' flag to true.
+ * 4. Calls 'musicButtonOnOff' to manage music control buttons.
+ * 5. Calls 'reset' to reset the game state.
+ * 6. Calls 'start' to start the game again.
+ */
 playAgainButtonYouLost.addEventListener("click", () => {
     buttonsTop.classList.remove("d-none");
     buttonsTop.classList.remove("buttons-top-now-show");
@@ -107,6 +167,11 @@ playAgainButtonYouLost.addEventListener("click", () => {
     start();
 });
 
+/**
+ * Toggles the state of the music control buttons.
+ * If the 'musicOffButton' is hidden, it shows it and sets 'soundOn' to true.
+ * If the 'musicOnButton' is hidden, it shows 'musicOffButton' and hides 'musicOnButton', setting 'soundOn' to false.
+ */
 const musicButtonOnOff = () => {
     if (musicOffButton.classList.contains("d-none")) {
         musicOffButton.classList.add("d-none");
@@ -118,6 +183,16 @@ const musicButtonOnOff = () => {
     }
 };
 
+/**
+ * Adds a click event listener to the 'playAgainButtonYouWon' element.
+ * When the button is clicked, it performs several actions to reset the game after winning:
+ * 1. Removes the "d-none" class from 'buttonsTop', 'buttonsBottom', 'playAgainButtonYouLost', and 'playAgainButtonYouWon', if present.
+ * 2. Removes the "buttons-top-now-show" and "buttons-bottom-now-show" classes from 'buttonsTop' and 'buttonsBottom', respectively.
+ * 3. Sets 'isGameOn' flag to true.
+ * 4. Calls 'musicButtonOnOff' to manage music control buttons.
+ * 5. Calls 'reset' to reset the game state.
+ * 6. Calls 'start' to start the game again.
+ */
 playAgainButtonYouWon.addEventListener("click", () => {
     buttonsTop.classList.remove("d-none");
     buttonsTop.classList.remove("buttons-top-now-show");
@@ -132,6 +207,16 @@ playAgainButtonYouWon.addEventListener("click", () => {
     start();
 });
 
+/**
+ * Toggles the break mode.
+ * This function toggles the visibility of break-related elements and manages game state.
+ * - Toggles the visibility of 'breakButton' and 'startButtonBreak'.
+ * - Toggles the 'play-again-show' class on 'breakInfo'.
+ * - Calls 'musicButtonDisable' to manage music control buttons.
+ * - Toggles the 'isGameOn' flag to indicate the game state.
+ * - Toggles the 'soundOn' flag to indicate whether sound is on or off.
+ *   If music is off, 'soundOn' is set to false regardless of the current state.
+ */
 const toggleBreak = () => {
     breakButton.classList.toggle("d-none");
     breakInfo.classList.toggle("play-again-show");
@@ -144,6 +229,16 @@ const toggleBreak = () => {
     }
 };
 
+/**
+ * Adds a click event listener to the 'breakInfo' element.
+ * When the 'breakInfo' element is clicked, it performs several actions:
+ * 1. Removes the "play-again-show" class from 'breakInfo', if present.
+ * 2. Removes the "d-none" class from 'breakButton', making it visible.
+ * 3. Adds the "d-none" class to 'startButtonBreak', hiding it.
+ * 4. Calls the 'musicButtonDisable' function to manage music control buttons.
+ * 5. Sets 'isGameOn' flag to true.
+ * 6. Checks if the music is on or off and updates the 'soundOn' flag accordingly.
+ */
 breakInfo.addEventListener("click", () => {
     breakInfo.classList.remove("play-again-show");
     breakButton.classList.remove("d-none");
@@ -157,9 +252,24 @@ breakInfo.addEventListener("click", () => {
     }
 });
 
+/**
+ * Adds a click event listener to the 'breakButton' element.
+ * When the 'breakButton' element is clicked, it invokes the 'toggleBreak' function.
+ */
 breakButton.addEventListener("click", toggleBreak);
+
+/**
+ * Adds a click event listener to the 'startButtonBreak' element.
+ * When the 'startButtonBreak' element is clicked, it invokes the 'toggleBreak' function.
+ */
 startButtonBreak.addEventListener("click", toggleBreak);
 
+/**
+ * Toggles the disabled attribute of music control buttons.
+ * If either 'musicOnButton' or 'musicOffButton' has the 'disabled' attribute,
+ * it removes the attribute from both buttons, enabling them.
+ * Otherwise, it sets the 'disabled' attribute to true for both buttons, disabling them.
+ */
 const musicButtonDisable = () => {
     if (
         musicOnButton.hasAttribute("disabled") ||
@@ -173,16 +283,34 @@ const musicButtonDisable = () => {
     }
 };
 
+/**
+ * Adds a click event listener to the 'fullScreen' element.
+ * When the 'fullScreen' element is clicked, it adds the 'd-none' class to the 'fullScreen' element,
+ * effectively hiding it.
+ * It also removes the 'd-none' class from the 'fullScreenOff' element, making it visible.
+ */
 fullScreen.addEventListener("click", () => {
     fullScreen.classList.add("d-none");
     fullScreenOff.classList.remove("d-none");
 });
 
+/**
+ * Adds a click event listener to the 'fullScreenOff' element.
+ * When the 'fullScreenOff' element is clicked, it removes the 'd-none' class from the 'fullScreen' element,
+ * making the 'fullScreen' element visible.
+ * It also adds the 'd-none' class to the 'fullScreenOff' element, effectively hiding it.
+ */
 fullScreenOff.addEventListener("click", () => {
     fullScreen.classList.remove("d-none");
     fullScreenOff.classList.add("d-none");
 });
 
+/**
+ * Adds a click event listener to the 'infoButtonBottom' element.
+ * When the 'infoButtonBottom' element is clicked, it toggles the 'd-none' class on the 'infoScreen' element,
+ * effectively showing or hiding the 'infoScreen'.
+ * If the game is currently on (indicated by 'isGameOn'), it calls the 'toggleBreak' function.
+ */
 infoButtonBottom.addEventListener("click", () => {
     infoScreen.classList.toggle("d-none");
     if (isGameOn) {
@@ -190,10 +318,22 @@ infoButtonBottom.addEventListener("click", () => {
     }
 });
 
+/**
+ * Adds a click event listener to the 'infoButtonTop' element.
+ * When the 'infoButtonTop' element is clicked, it toggles the 'd-none' class on the 'infoScreen' element,
+ * effectively showing or hiding the 'infoScreen'.
+ */
 infoButtonTop.addEventListener("click", () => {
     infoScreen.classList.toggle("d-none");
 });
 
+/**
+ * Adds a click event listener to the 'closeButton' element.
+ * When the 'closeButton' element is clicked, it toggles the 'd-none' class on the 'infoScreen' element,
+ * effectively showing or hiding the 'infoScreen'.
+ * If the 'startScreen' element contains the 'd-none' class (i.e., it is hidden),
+ * and the game is currently on (indicated by 'isGameOn'), it calls the 'toggleBreak' function.
+ */
 closeButton.addEventListener("click", () => {
     infoScreen.classList.toggle("d-none");
     if (startScreen.classList.contains("d-none")) {
@@ -203,10 +343,20 @@ closeButton.addEventListener("click", () => {
     }
 });
 
+/**
+ * Adds a click event listener to the 'startScreen' element.
+ * When the 'startScreen' element is clicked, the 'd-none' class is added to the 'infoScreen' element,
+ * effectively hiding the 'infoScreen' by applying the 'd-none' class.
+ */
 startScreen.addEventListener("click", () => {
     infoScreen.classList.add("d-none");
 });
 
+/**
+ * Function to toggle the fullscreen mode.
+ * If the document is currently in fullscreen mode, exits fullscreen.
+ * If the document is not in fullscreen mode, requests fullscreen for the 'playGame' element.
+ */
 const toggleFullscreen = () => {
     if (document.fullscreenElement) {
         document.exitFullscreen();
@@ -215,9 +365,24 @@ const toggleFullscreen = () => {
     }
 };
 
+/**
+ * Adds a click event listener to the 'fullScreen' element.
+ * When the 'fullScreen' element is clicked, the 'toggleFullscreen' function will be executed.
+ */
 fullScreen.addEventListener("click", toggleFullscreen);
+
+/**
+ * Adds a click event listener to the 'fullScreenOff' element.
+ * When the 'fullScreenOff' element is clicked, the 'toggleFullscreen' function will be executed.
+ */
 fullScreenOff.addEventListener("click", toggleFullscreen);
 
+/**
+ * Function to handle the 'fullscreenchange' event.
+ * Checks if the 'playGame' element exists and is currently in fullscreen mode.
+ * If in fullscreen mode, sets the class name of 'playGame' to "fullscreen".
+ * If not in fullscreen mode, sets the class name of 'playGame' to "play-game" or any class that represents the normal state.
+ */
 const onChange = () => {
     if (playGame) {
         if (document.fullscreenElement) {
@@ -228,4 +393,9 @@ const onChange = () => {
     }
 };
 
+/**
+ * Adds an event listener to detect changes in the fullscreen state of the document.
+ * The 'fullscreenchange' event is triggered when the document enters or exits fullscreen mode,
+ * and the 'onChange' function will be executed in response to these changes.
+ */
 document.addEventListener("fullscreenchange", onChange);
