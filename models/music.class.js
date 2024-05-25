b2a949a9244f60159ba189e528eecfc3f60adcd2
Damian Poloczek <damianpoloczek@gmail.com>
2024-05-25 22:11:40 +0200

    music.class.js docs

--- models/music.class.js
+++ models/music.class.js
@@ -21,20 +21,38 @@ class SoundManager {
         };
     }
 
+    /**
+     * Plays the specified sound with optional volume control.
+     * @param {string} sound - The key of the sound in the 'sounds' object.
+     * @param {number} [volume=1] - The volume level (default is 1).
+     * @returns {void}
+     */
     playSound(sound, volume = 1) {
+        // If sound is off, return early
         if (!soundOn) {
             return;
         } else {
+            // Set the volume and play the specified sound
             this.sounds[sound].volume = volume;
             this.sounds[sound].play();
         }
     }
 
+    /**
+     * Pauses the specified sound.
+     * @param {string} sound - The key of the sound in the 'sounds' object.
+     */
     pauseSound(sound) {
+        // Pause the specified sound
         this.sounds[sound].pause();
     }
 
+    /**
+     * Resets the playback position of the specified sound to the beginning.
+     * @param {string} sound - The key of the sound in the 'sounds' object.
+     */
     resetSound(sound) {
+        // Set the current playback time of the specified sound to 0
         this.sounds[sound].currentTime = 0;
     }
 }
